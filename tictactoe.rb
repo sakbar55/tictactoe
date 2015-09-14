20b3659cfb8967fb112e54ba4ceb1f32287c131f
Sanam Akbar <akbar.sanam@gmail.com>
2015-09-14 11:53:02 -0400

    first commit

---
+++ tictactoe.rb
@@ -0,0 +1,28 @@
+class Board
+
+  def initialize
+    @board = Array.new(3) { Array.new(3, " ") }
+  end
+
+  def printInstructions
+    puts "1 | 2 | 3",
+         "---------",
+         "4 | 5 | 6",
+         "---------",
+         "7 | 8 | 9"
+    print "\n"
+  end
+
+  def printBoard
+    (0..2).each do |row|
+      print "       "
+      (0..2).each do |col|
+        print @board[row][col]
+        print " | " unless col == 2
+      end
+      print "\n"
+      print "       ---------\n" unless row == 2
+    end
+    print "\n"
+  end
+end
